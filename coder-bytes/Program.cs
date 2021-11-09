07273efd0befa64147cbd8aa3c525e1201b15dbb
muraligummalla <murali.gummalla@yahoo.com>
2021-11-09 11:11:30 -0800

    Add project files.

---
+++ coder-bytes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace coder_bytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest request = WebRequest.Create("https://coderbyte.com/api/challenges/json/age-counting");
+            WebResponse response = request.GetResponse();
+
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string content = reader.ReadToEnd();
+            response.Close();
+
+            int count = content
+                .Split(':')[1]
+                .Trim('{', '}', '\"')
+                .Split(',')
+                .Where(x => x.Trim()
+                .Split('=')[0] == "age")
+                .Where(x => Convert.ToInt32(x.Split('=')[1]) > 50)
+                .Count();
+            Console.WriteLine(count);
+        }
+    }
+}
